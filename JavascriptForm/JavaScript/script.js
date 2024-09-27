9abed4af51edb91a428f53e90a5f4ce22039623e
Fatima Latif <fatemahlateef025@gmail.com>
2024-09-27 19:12:42 +0500

    display a message thanking the user for submitting the form, replacing the form fields

--- JavascriptForm/JavaScript/script.js
+++ JavascriptForm/JavaScript/script.js
@@ -1,8 +1,12 @@
-function changeFormColor() {
-  const form = document.getElementById("form");
-  const inputs = form.querySelectorAll("input");
-  const button = form.querySelector("button");
+const submitbtn = document.getElementById("submitbtn");
+const form = document.getElementById("form");
+const inputs = form.querySelectorAll("input");
+const button = form.querySelector("button");
+const showMessage = document.getElementById("show-thanks")
+const label = form.querySelectorAll('label')
+const fname = document.getElementById('fname')
 
+submitbtn.addEventListener("click", function (e) {
   form.style.backgroundColor = "#0e0e47";
   form.style.color = "white";
   form.style.fontWeight = "bold";
@@ -12,4 +16,16 @@ function changeFormColor() {
   inputs.forEach((input) => {
     input.style.color = "red";
   });
-}
+ inputs.forEach((input => {
+  input.style.display = "none"
+ }))
+ submitbtn.style.display = "none"
+ label.forEach(label => {
+  label.style.display = "none"
+ })
+ showMessage.style.display = 'block'
+});
+
+fname.addEventListener('input', (e)=>{   //for input event
+  console.log(e.target.value)
+})
